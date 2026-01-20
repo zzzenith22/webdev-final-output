43723e343895f45435ab278d76396d350101803b
zzzenith22 <johnzenithp.cruz@gmail.com>
2026-01-21 02:10:43 +0800

    added navigations

--- root/js/inner.js
+++ root/js/inner.js
@@ -4,37 +4,37 @@ fetch("assets/json/homepage.json")
     const container = document.getElementById("units-container");
 
     Object.values(data).forEach(unit => {
-
       container.innerHTML += `
-        <div class="unit_one">
+        <a href="${unit.page}" class="unit_one_link">
+          <div class="unit_one">
 
-            <video loop muted poster="${unit.mainpic}">
-                <source src="${unit.video}" type="video/mp4">
-            </video>
+              <video loop muted poster="${unit.mainpic}">
+                  <source src="${unit.video}" type="video/mp4">
+              </video>
 
-            <p class="unitone_details">${unit.details}</p>
-            <p class="name_of_unit">${unit.nameOfUnit}</p>
-            <p class="unitone_description">${unit.unitDescription}</p>
+              <p class="unitone_details">${unit.details}</p>
+              <p class="name_of_unit">${unit.nameOfUnit}</p>
+              <p class="unitone_description">${unit.unitDescription}</p>
 
-            <div class="unitone_location">
-                <img class="location_icon" src="${unit.location}">
-                <p class="location_name">${unit["location-name"]}</p>
-            </div>
+              <div class="unitone_location">
+                  <img class="location_icon" src="${unit.location}">
+                  <p class="location_name">${unit["location-name"]}</p>
+              </div>
 
-            <div class="unit_details">
-                <div class="guests">
-                    <img src="assets/picture/cake.png" class="location_icon">
-                    <p class="location_name">${unit.guests}</p>
-                </div>
+              <div class="unit_details">
+                  <div class="guests">
+                      <img src="assets/picture/cake.png" class="location_icon">
+                      <p class="location_name">${unit.guests}</p>
+                  </div>
 
-                <div class="beds">
-                    <img src="assets/picture/single-bed.png" class="location_icon">
-                    <p class="location_name">${unit.beds}</p>
-                </div>
-            </div>
-        </div>
+                  <div class="beds">
+                      <img src="assets/picture/single-bed.png" class="location_icon">
+                      <p class="location_name">${unit.beds}</p>
+                  </div>
+              </div>
+          </div>
+        </a>
       `;
-
     });
   })
   .catch(err => console.log("Error loading JSON:", err));
